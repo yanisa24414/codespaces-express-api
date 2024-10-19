b83c75a90116473319001334f78a5aed820bcfba
yanisa24414 <yanisa.sangj@ku.th>
2024-10-19 05:29:35 +0000

    Initial commit

--- index.js
+++ index.js
@@ -1,45 +1,107 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-const port = 5000
+const express = require('express');
+const bodyParser = require('body-parser');
+var mysql = require('mysql');
 
-app.use((req,res,next) => {
-  res.setHeader("Access-Control-Allow-Origin",+"*");
+const app = express();
+const port = 5001;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use((req,res,next)=>{
+  res.setHeader("Access-Control-Allow-Origin","*");
   res.setHeader(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept,Authorization",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization",
   );
   res.setHeader(
     "Access-Control-Allow-Methods",
-    "GET , POST,PUT,DELETE "
+    "GET, POST, PUT, DELETE",
   );
   next();
 });
+app.use(express.json());
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(express.json())
+var con = mysql.createConnection({
+  host: "korawit.ddns.net",
+  user: "webapp",
+  password: "secret2024",
+  port: "3307",
+  database: "shop",
+});
 
-const products = [
-  { id: 0, name: "Notebook Acer Swift", price: 45900, img: "https://img.advice.co.th/images_nas/pic_product4/A0147295/A0147295_s.jpg" },
-  { id: 1, name: "Notebook Asus Vivo", price: 19900, img: "https://img.advice.co.th/images_nas/pic_product4/A0146010/A0146010_s.jpg" },
-  { id: 2, name: "Notebook Lenovo Ideapad", price: 32900, img: "https://img.advice.co.th/images_nas/pic_product4/A0149009/A0149009_s.jpg" },
-  { id: 3, name: "Notebook MSI Prestige", price: 54900, img: "https://img.advice.co.th/images_nas/pic_product4/A0149954/A0149954_s.jpg" },
-  { id: 4, name: "Notebook DELL XPS", price: 99900, img: "https://img.advice.co.th/images_nas/pic_product4/A0146335/A0146335_s.jpg" },
-  { id: 5, name: "Notebook HP Envy", price: 46900, img: "https://img.advice.co.th/images_nas/pic_product4/A0145712/A0145712_s.jpg" }
-];
+con.connect(function(err){
+  if(err) throw err;
+});
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!');
 })
 
-app.get('/api/products', (req, res) => {
-  if (products.length > 0)
-    res.send(products);
-  else
-    res.status(400).send("No products found");
+app.get('/api/products',(req,res)=>{
+  con.query("SELECT * FROM products",function(err,result,fields){
+    if(err) throw res.status(400).send("No products found");
+    console.log(result);
+    res.send(result);
+  });
 })
+
+app.get('/api/products/:id',(req,res)=>{
+  const id = req.params.id;
+  con.query(`SELECT * FROM products where id=${id}`,function(err,result,fields){
+    if(err) throw err;
+    if(result.length==0) 
+      res.status(400).send(`No products id: ${id} found`);
+    else{
+      console.log(result);
+      res.send(result);
+    }
+  });
+});
+
+app.post('/api/addproduct',(req,res)=>{
+  const name=req.body.name;
+  const price=req.body.price;
+  const img=req.body.img;
+  console.log(name,price,img);
+  con.query(`INSERT INTO products (name,price,img) VALUES('${name}','${price}','${img}')`,function(err,result,fields){
+    if(err) throw res.status(400).send("Error, cannot add product");
+    con.query("SELECT * FROM products",function(err,result,fields){
+      if(err) throw res.status(400).send("No products found");
+      console.log(result);
+      res.send({products:result,status:"ok"});
+    });
+    });
+});
+
+app.delete('/api/delproduct/:id',(req,res)=>{
+  const id =req.params.id;
+  con.query(`DELETE FROM products WHERE id=${id}`,function(err,result,fields){
+    if(err) throw res.status(400).send("Error, cannot delete product");
+    con.query("SELECT * FROM products",function(err,result,fields){
+      if(err) throw res.status(400).send("No products found");
+      console.log(result);
+      res.send({products:result,status:"ok"});
+    });
+    });
+});
+
+app.put('/api/updateproduct/:id',(req,res)=>{
+  const id =req.params.id;
+  const name=req.body.name;
+  const price=req.body.price;
+  const img=req.body.img;
+  console.log(id,name,price,img)
+  con.query(`UPDATE products SET name='${name}',price='${price}',img='${img}' WHERE id=${id}`,function(err,result,fields){
+    if(err) throw res.status(400).send("Error, cannot update product");
+    con.query("SELECT * FROM products",function(err,result,fields){
+      if(err) throw res.status(400).send("No products found");
+      console.log(result);
+      res.send({products:result,status:"ok"});
+    });
+    });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
